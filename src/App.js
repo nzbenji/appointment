9fa6d7e8cd49d6af20d744b5e7323cf4167936b1
nzbenji <benlittle53@gmail.com>
2019-02-02 17:44:22 +1300

    added sign in form data

--- src/App.js
+++ src/App.js
@@ -1,16 +1,49 @@
 import React, { Component } from 'react';
 import {Route} from 'react-router-dom';
+//import { Container } from 'semantic-ui-react';
+import {Link} from 'react-router-dom';
+
 import './App.css';
 
 import View from './components/View'
 import Home from './components/Home'
+import Signin from './components/SIgnin/Signin'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      route: 'signin'
+    }
+  }
+
+  onRouteChange = () => {
+    this.setState({
+      route: 'home'
+    })
+  }
   render() {
+    console.log(this.state.route)
     return (
       <div className="App">
+      { this.state.route === 'signin' 
+        ?
+        <Link to={"/login"}>Sign in</Link>
+        :
+        <Link to={'/signout'}>Sign out</Link>
+    }
+      
+      
+
+      <Route exact path="/login" 
+      component={Signin} 
+      render={() => <onRouteChange onRouteChange={this.onRouteChange} />} 
+      />
+      <Route exact path="/signout" />
       <Route exact path="/" component={Home} />
       <Route exact path="/appointment" component={View} />
+
         
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import {HashRouter as Router} from 'react-router-dom'
+import 'semantic-ui-css/semantic.min.css'
 
 ReactDOM.render(
   <Router>
