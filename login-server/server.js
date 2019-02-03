0cae17ec0a407f8385edb5a5c2b95dd6667c3627
nzbenji <benlittle53@gmail.com>
2019-02-03 13:48:59 +1300

    added routes and dummy data

--- login-server/server.js
+++ login-server/server.js
@@ -1,10 +1,64 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const server = express()
+
+server.use(bodyParser.json())
 
 const PORT = 3000
 
+const db = {
+    users: [
+        {
+            id: '1', 
+            name: 'john',
+            email: 'john@john.com',
+            password: '1234'
+        },
+        {
+            id: '2', 
+            name: 'sally',
+            email: 'sally@sally.com',
+            password: '4321'
+        }
+    ]
+}
+
 server.get('/', (req, res) => {
-    res.send('this is home ')
+    res.send(db.users)
+})
+server.post('/login', (req, res) => {
+    if(req.body.email === db.users[0].email &&
+        req.body.password === db.users[0].password) {
+            res.json('success')
+        } else {
+            res.status(400).json('err loggin in')
+        }
+})
+
+server.get('/profile/:id', (req, res) => {
+    const { id } = req.params
+    let found = false
+    db.users.forEach(user => {
+        if(user.id === id) {
+            found = true
+            return res.json(user)
+        }
+    })
+    if(!found) {
+        res.status(404).json('no such user')
+    }
+})
+
+server.post('/register', (req, res) => {
+    const { email, name, password } = req.body
+    db.users.push({
+        id: '123', 
+        name: name,
+        email: email,
+        password: password
+    })
+    //grabs last added item in arr
+    res.json(db.users[db.users.length-1])
 })
 
 server.listen(PORT, () => {
